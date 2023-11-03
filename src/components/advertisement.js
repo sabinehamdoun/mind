fbe03ec762f1be5f3f2ec2c74d7bde72fec59496
sabinehamdoun <sabine.hamdoun@lau.edu>
2023-11-03 05:49:13 +0200

    s

--- src/components/advertisement.js
+++ src/components/advertisement.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Card from "./card"; 
-import axios from 'axios';
+import Card from "./card";
+import axios from "axios";
 import Link from "next/link";
 
 const CardGrid = () => {
@@ -9,38 +9,36 @@ const CardGrid = () => {
   const fetchLinksData = async () => {
     await axios
       .get("/api/fetchLinks")
-      .then((res) => { 
+      .then((res) => {
         setData(res.data.data);
       })
       .catch(console.error);
   };
-  
+
   useEffect(() => {
     fetchLinksData();
   }, []);
 
-  console.log(data)
+  console.log(data);
 
   return (
     <div className="2xl:container mx-auto px-5 sm:px-12 lg:px-28 py-20">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-8">
-      {data && data.map((card) => (
-  // <Link
-  //   key={card.id}
-  //   href={`/detail?cardId=${index}`} 
-  // >
-  <Link
-  key={card.id}
-  href={`/projects/${card.id}`} 
->
-    <Card
-    //  key={card.id}
-      imageSrc={card.image}
-      title={card.title}
-      description={card.text}
-    />
-   </Link>
-))}
+        {data &&
+          data.map((card) => (
+            // <Link
+            //   key={card.id}
+            //   href={`/detail?cardId=${index}`}
+            // >
+            <Link key={card.id} href={`/projects/${card.id}`}>
+              <Card
+                //  key={card.id}
+                imageSrc={card.image}
+                title={card.title}
+                description={card.text}
+              />
+            </Link>
+          ))}
       </div>
     </div>
   );
--- src/pages/api/fetchLinkById.js
+++ src/pages/api/fetchLinkById.js
@@ -1,17 +1,17 @@
-export default async function handler(req, res) {
-    try {
-      const { query: { projectId } } = req; 
+// export default async function handler(req, res) {
+//     try {
+//       const { query: { projectId } } = req; 
   
-      const headers = {
-        'Accept-Language': 'en',
-      };
+//       const headers = {
+//         'Accept-Language': 'en',
+//       };
   
-      const apiURL = `${process.env.NEXT_PUBLIC_BASE_URL}/projects/${projectId}`;
-      const response = await axios.get(apiURL, { headers });
-      const projectData = response.data;
-      res.status(200).json(projectData);
-    } catch (error) {
-      console.error('API request failed:', error);
-      res.status(500).json({ error: 'API request failed' });
-    }
-  }
+//       const apiURL = `${process.env.NEXT_PUBLIC_BASE_URL}/projects/${projectId}`;
+//       const response = await axios.get(apiURL, { headers });
+//       const projectData = response.data;
+//       res.status(200).json(projectData);
+//     } catch (error) {
+//       console.error('API request failed:', error);
+//       res.status(500).json({ error: 'API request failed' });
+//     }
+//   }
