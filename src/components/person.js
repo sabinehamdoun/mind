942906e935d2f1c142356a6d92642ca1048491a0
sabinehamdoun <sabine.hamdoun@lau.edu>
2023-11-07 07:58:56 +0200

    s

--- src/components/person.js
+++ src/components/person.js
@@ -12,16 +12,14 @@ const Person = ({ imageSrc, borderColor, bgColor, name, role }) => {
           className="h-36 w-36 rounded-full mx-auto top-1/2 transform translate-y-1.5 object-cover"
         />
       </div>
-      <div className="mx-auto flex transform translate-x-[47%] ">
+      <div className="mx-auto flex transform translate-x-[47%] md:translate-x-[44%] lg:translate-x-[47%] ">
         <div className="flex flex-col items-center">
           <div className={`border-l ${borderColor} h-32`}></div>
           <div className={`w-3 h-3 ${bgColor} rounded-full`}></div>
         </div>
         <div className="mt-2 pr-44 md:pr-32">
-          {name && <div className="font-light text-xs sm:text-sm">{name}</div>}
-          {role && (
-            <div className="font-semibold text-xs sm:text-sm">{role}</div>
-          )}
+          <div className="font-light text-xs sm:text-sm">{name}</div>
+          <div className="font-semibold text-xs sm:text-sm">{role}</div>
         </div>
       </div>
     </div>
